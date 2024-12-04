4cb209f754c9da9c7fe38e52d7b9bd1ad7f5a307
prashant <prashant37364@gmail.com>
2024-12-05 00:20:20 +0530

    the changes hs been made

---
+++ basics/variable.js
@@ -0,0 +1,41 @@
+// var x = 10; 
+//  if(true){
+//     console.log(x); 
+//  }
+//   if i create a var in fucntion and try to acces globally is not avaiblable 
+// var keyword is function scoped 
+// it also give global access and debugging  issue 
+function solve(){
+    var age = 10;
+    var age = 20;
+    
+ }
+ // console.log(age);
+ solve();
+ 
+ 
+ // let variable
+ // we cannot use the same name of the varibale again in let 
+ let age = 10;
+  console.log(age);
+ 
+  // if declared in blocked brackets the output code must be written there too
+   {
+    let vote = 20;
+    console.log(vote);
+   } 
+   // if you write console .log oustside it wont work .. 
+ 
+ 
+ 
+ 
+ 
+ //  --->  const variable --> 
+  
+ 
+ const n = 10;
+ console.log(n);
+  
+ // const n = 20;--> cannot be declared again 
+ 
+ 
